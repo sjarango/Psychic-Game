7b65bb1dd0f93839edddf1fd93ebb790c0b166f4
Sebastian Arango <sjarango92@gmail.com>
2019-08-24 20:21:04 -0400

    changed checkWin to gameLogic, added update(), commented out console.log statements expect for winning answer

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -7,7 +7,7 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 
 // global selected letter 
 var letter = randomLetter();
-console.log("letter is: " + letter);
+console.log("letter is: " + letter); // check for winning letter in console
 
 const limit = 9;
 var guessesLeft = limit;
@@ -30,56 +30,69 @@ targetDiv.textContent = "Guesses so far: " + guesses;
 var userInput;
 document.onkeyup = function(event) {
     userInput = event.key;
-    console.log("user input is: " + userInput);
+    //console.log("user input is: " + userInput);
     
-    checkWin();
+    gameLogic();
 
 }
 
 // random letter generator
 function randomLetter() {
-    randomNum = Math.floor(Math.random() * 3); //change to 26 for final product, set to 3 to test
+    randomNum = Math.floor(Math.random() * 26); //change to 26 for final product, set to 3 to test (a,b,c)
     return letters[randomNum];
 }
 
 // main logic for game
-function checkWin() {
+function gameLogic() {
     // checks for win
     if( letter == userInput ){
-        console.log("its a win");
+        //console.log("its a win");
         wins++;
         reset();
     } else {
         // operations for guesses-left and concat strings for guesses-so-far
         guessesLeft--;
-        console.log("guesses left: " + guessesLeft);
+        //console.log("guesses left: " + guessesLeft);
         if(guessesLeft == 8){
             guesses = userInput;
-            console.log("guesses so far: " + guesses);
+            //console.log("guesses so far: " + guesses);
         } else {
             str1 = guesses;
             str2 = ", " + userInput;
             guesses = str1.concat(str2);
-            console.log("guesses so far: " + guesses);
+            //console.log("guesses so far: " + guesses);
         }
     }
     // checks for loss
     if (guessesLeft < 1){
-        console.log("its a loss");
+        //console.log("its a loss");
         loss++;
         reset();
     }
+    update();
+}
+
+// updates page content
+function update() {
+    targetDiv = document.getElementById("wins");
+    targetDiv.textContent = "Wins: " + wins;
+    targetDiv = document.getElementById("losses");
+    targetDiv.textContent = "Losses: " + loss;
+    targetDiv = document.getElementById("guesses-left");
+    targetDiv.textContent = "Guesses left: " + guessesLeft;
+    targetDiv = document.getElementById("guesses-so-far");
+    targetDiv.textContent = "Guesses so far: " + guesses;
 }
 
 // resets all variables, except for wins & losses 
 function reset() {
-    console.log("wins: " + wins);
-    console.log("losses: " + loss);
+    //console.log("wins: " + wins);
+    //console.log("losses: " + loss);
     letter = randomLetter();
-    console.log("new letter is: " + letter);
+    console.log("new letter is: " + letter); // check for winning letter in console
     guessesLeft = limit;
-    console.log("guesses left: " + guessesLeft);
+    //console.log("guesses left: " + guessesLeft);
     guesses = '';
     var str1, str2 = '';
-    console.log("guesses so far: " + guesses);
+    //console.log("guesses so far: " + guesses);
 }
